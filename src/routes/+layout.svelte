83d22c91faf192cd3b797e7cbbd0941d2d8b495b
LeroyBG <leroylightning@g.ucla.edu>
2024-06-27 16:04:35 -0700

    fix menu bar for phones?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,9 @@ import "../app.css"
     <div class="h-20">
         <nav class="w-3/4 h-full">
             <ul class="flex items-center h-full text-theme-dark">
-                <li><a href="/#" class="hover:underline font-normal px-5 text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">home</a></li>
-                <li><a href="/skeleton" class="hover:underline font-normal px-5 text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">skeleton</a></li>
-                <li><a href="/this-site" class="hover:underline font-normal px-5 text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">this site</a></li>
+                <li class="grow max-w-32 ml-3"><a href="/#"            class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">home</a></li>
+                <li class="grow max-w-32"><a href="/skeleton"     class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">skeleton</a></li>
+                <li class="grow max-w-32"><a href="/this-site"    class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">this site</a></li>
             </ul>
         </nav>
     </div>
