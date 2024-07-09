cc5b4a203afd53a57f10ae37ac841a4b4489d43b
LeroyBG <leroylightning@g.ucla.edu>
2024-07-08 21:14:21 -0400

    svelte is wonderful... at least for now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,19 @@
 <script>
 import "../app.css"
+
+const navItems = [
+    ["home", "/#"],
+    ["skeleton", "/skeleton"],
+    ["this site", "/this-site"]
+]
 </script>
 <div class="min-h-screen bg-cherryPearl-500 flex flex-col">
     <div class="h-20">
         <nav class="w-3/4 h-full">
-            <ul class="flex items-center h-full text-theme-dark">
-                <li class="grow max-w-32 ml-3"><a href="/#"            class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">home</a></li>
-                <li class="grow max-w-32"><a href="/skeleton"     class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">skeleton</a></li>
-                <li class="grow max-w-32"><a href="/this-site"    class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">this site</a></li>
+            <ul class="flex items-center h-full text-theme-dark ml-3">
+                {#each navItems as navItem}
+                <li class="grow max-w-32"><a href={navItem[1]} class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">{navItem[0]}</a></li>
+                {/each}
             </ul>
         </nav>
     </div>
