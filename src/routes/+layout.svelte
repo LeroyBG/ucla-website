51c3a01ee34c7be7622567485a26dd76296e23a1
LeroyBG <leroylightning@g.ucla.edu>
2025-05-30 14:36:12 -0700

    fix wrong nav item opening in new tab

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,8 +13,8 @@ const navItems = [
         <nav class="w-3/4 h-full">
             <ul class="flex items-center h-full text-theme-dark ml-3">
                 <li class="grow max-w-32"><a href="/#" class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">home</a></li>
-                <li class="grow max-w-32"><a href={PUBLIC_SKELETON_DEPLOY_URL} class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">skeleton</a></li>
-                <li class="grow max-w-32"><a href="/this-site" target="_blank" class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">this site</a></li>
+                <li class="grow max-w-32"><a href={PUBLIC_SKELETON_DEPLOY_URL} target="_blank" class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">skeleton</a></li>
+                <li class="grow max-w-32"><a href="/this-site" class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">this site</a></li>
             </ul>
         </nav>
     </div>
