3011b2d4b998cdac8fbc6ce56fb96f075c614c92
LeroyBG <leroylightning@g.ucla.edu>
2025-05-29 23:02:43 -0700

    remove skeleton iframe and make it a link

--- src/routes/skeleton/+page.svelte
+++
@@ -1,3 +0,0 @@
-<div id="skeleton container" class="w-full grow flex flex-col items-center">
-    <iframe title="skeleton" class="w-full lg:w-5/6 grow border-8 border-skeletonGreen-900" src="https://skeleton-lac.vercel.app"></iframe>
-</div>
