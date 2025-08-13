1cfe631de9b47291c30cb28a49771159adb7f065
LeroyBG <leroylightning@g.ucla.edu>
2025-08-13 12:44:56 -0700

    use svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,12 @@
-<script>
-import "../app.css"
-import { PUBLIC_SKELETON_DEPLOY_URL } from "$env/static/public"
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
 
-const navItems = [
-    ["home", "/#"],
-    ["skeleton", PUBLIC_SKELETON_DEPLOY_URL],
-    ["this site", "/this-site"]
-]
+	let { children } = $props();
 </script>
-<div class="min-h-screen flex flex-col">
-    <div class="h-20">
-        <nav class="w-3/4 h-full">
-            <ul class="flex items-center h-full text-theme-dark ml-3">
-                <li class="grow max-w-32"><a href="/#" class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">home</a></li>
-                <li class="grow max-w-32"><a href={PUBLIC_SKELETON_DEPLOY_URL} target="_blank" class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">skeleton</a></li>
-                <li class="grow max-w-32"><a href="/this-site" class="hover:underline font-normal text-2xl decoration-nettleRash-500 underline-offset-4 decoration-8">this site</a></li>
-            </ul>
-        </nav>
-    </div>
-    <div class="w-full grow flex flex-col h-full">
-        <slot/>
-    </div>
-</div>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children?.()}
